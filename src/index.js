98d17a2bb2a384d3ad2a9d4f1f8548f252d7dec1
Kseniya <kseniyabialkevich@gmail.com>
2019-03-23 22:29:36 +0300

    fear: Solution.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,45 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences) {
+  preferences = preferences || [];
   // your implementation
+  return calcLoveTriangles(preferences);
 };
+
+var calcLoveTriangles = function(preferences) {
+  var p = preferences;
+  var triangles = [];
+
+  for (var i=0; i<preferences.length; i++) {
+    var fl = p[i];
+    var sl = p[p[i] - 1];
+    var tl = p[p[p[i] - 1] - 1];
+    var thirdLove = tl - 1;
+    if (thirdLove == i) triangles.push([fl, sl, tl].sort());
+  }
+
+  var res = new Map();
+  for (var i=0; i<triangles.length; i++) {
+    var triangle = triangles[i].join("");
+    var count = res.get(triangle);
+
+    if (count) {
+      res.set(triangle,++count);
+    }
+    else {
+      res.set(triangle,1);
+    }
+  }
+
+  var cnt = 0;
+  for(t of res) {
+    if (t[1] > 1) cnt++;
+  }
+
+  return cnt;
+}
+// calcLoveTriangles([2, 3, 1]);
+// calcLoveTriangles([2, 3, 1, 5, 6, 4]);
+
+
